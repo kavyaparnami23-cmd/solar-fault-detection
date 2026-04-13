740c7d2bb6d80f8709ec23b3b94daa7e84968475
kavyaparnami23-cmd <kavya.parnami23@st.niituniversity.in>
2026-04-13 17:54:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,34 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
 from PIL import Image
+from tflite_runtime.interpreter import Interpreter
 
 IMG_SIZE = (224, 224)
 class_names = ['clean', 'crack', 'dust']
 
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model("final_model.h5")
-                                       
-    return model
+    interpreter = Interpreter(model_path="model.tflite")
+    interpreter.allocate_tensors()
+    return interpreter
 
-model = load_model()
+interpreter = load_model()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 def preprocess_image(image):
     image = image.resize(IMG_SIZE)
-    img = np.array(image).astype(np.float32)
-
-    img = tf.keras.applications.efficientnet.preprocess_input(img)
-
+    img = np.array(image).astype(np.float32) / 255.0
     img = np.expand_dims(img, axis=0)
     return img
 
 def predict(image):
     img = preprocess_image(image)
-    preds = model.predict(img, verbose=0)[0]
-    return preds
-
-st.set_page_config(page_title="Solar Fault Detection", layout="centered")
+    interpreter.set_tensor(input_details[0]['index'], img)
+    interpreter.invoke()
+    output = interpreter.get_tensor(output_details[0]['index'])
+    return output[0]
 
 st.title("Solar Panel Fault Detection ⚡")
 st.write("Upload an image to detect Clean / Crack / Dust")
@@ -37,7 +37,6 @@ uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     preds = predict(image)
@@ -48,18 +47,16 @@ if uploaded_file is not None:
 
     if final_pred == "DUST":
         action = "Cleaning Required 🧹"
-        st.error(f"Prediction: {final_pred}")
     elif final_pred == "CRACK":
         action = "Maintenance Required ⚠️"
-        st.warning(f"Prediction: {final_pred}")
     else:
         action = "No Action Needed ✅"
-        st.success(f"Prediction: {final_pred}")
 
+    st.subheader(f"Prediction: {final_pred}")
     st.write(f"Confidence: {confidence}%")
     st.write(f"Action: {action}")
 
-    st.markdown("### 📊 Probabilities")
+    st.write("### Probabilities:")
     st.write(f"Clean: {preds[0]*100:.2f}%")
     st.write(f"Crack: {preds[1]*100:.2f}%")
     st.write(f"Dust: {preds[2]*100:.2f}%")
