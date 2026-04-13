179a9623207c18fe5312471b476d0c4592433347
kavyaparnami23-cmd <kavya.parnami23@st.niituniversity.in>
2026-04-13 20:43:22 +0530

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -4,7 +4,9 @@ import tensorflow as tf
 from PIL import Image
 
 IMG_SIZE = (224, 224)
-class_names = ['clean', 'crack', 'dust']
+
+DUST_THRESHOLD = 75
+CRACK_THRESHOLD = 60
 
 @st.cache_resource
 def load_model():
@@ -16,48 +18,51 @@ model = load_model()
 def preprocess_image(image):
     image = image.resize(IMG_SIZE)
     img = np.array(image).astype(np.float32)
-
-    img = img / 255.0  # use this if NOT trained with EfficientNet preprocess
-
+    img = tf.keras.applications.efficientnet.preprocess_input(img)
     img = np.expand_dims(img, axis=0)
     return img
 
 def predict(image):
     img = preprocess_image(image)
-    preds = model.predict(img, verbose=0)[0]
+    preds = model.predict(img)[0]
     return preds
 
-st.set_page_config(page_title="Solar Fault Detection", layout="centered")
+st.set_page_config(page_title="Solar Fault Detection", page_icon="⚡")
 
-st.title("Solar Panel Fault Detection ⚡")
-st.write("Upload an image to detect Clean / Crack / Dust")
+st.title("⚡ Solar Panel Fault Detection")
 
 uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
 
-if uploaded_file is not None:
+if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, use_column_width=True)
 
     preds = predict(image)
 
-    pred_index = np.argmax(preds)
-    final_pred = class_names[pred_index].upper()
-    confidence = round(preds[pred_index] * 100, 2)
+    clean_prob = preds[0] * 100
+    crack_prob = preds[1] * 100
+    dust_prob = preds[2] * 100
 
-    if final_pred == "DUST":
-        action = "Cleaning Required"
-        st.error(f"Prediction: {final_pred}")
-    elif final_pred == "CRACK":
-        action = "Maintenance Required"
-        st.warning(f"Prediction: {final_pred}")
+    if dust_prob > DUST_THRESHOLD:
+        final_pred = "DUST"
+        action = " Cleaning Required"
+        confidence = dust_prob
+
+    elif crack_prob > CRACK_THRESHOLD:
+        final_pred = "CRACK"
+        action = " Maintenance Required"
+        confidence = crack_prob
+
     else:
-        action = "No Action Needed"
-        st.success(f"Prediction: {final_pred}")
+        final_pred = "CLEAN"
+        action = " No Action Needed"
+        confidence = clean_prob
 
-    st.write(f"Confidence: {confidence}%")
+    st.success(f"Prediction: {final_pred}")
+    st.write(f"Confidence: {confidence:.2f}%")
     st.write(f"Action: {action}")
 
-    st.markdown("### Probabilities")
-    st.write(f"Clean: {preds[0]*100:.2f}%")
-    st.write(f"Crack: {preds[1]*100:.2f}%")
-    st.write(f"Dust: {preds[2]*100:.2f}%")
+    st.write("### Probabilities:")
+    st.write(f"Clean: {clean_prob:.2f}%")
+    st.write(f"Crack: {crack_prob:.2f}%")
+    st.write(f"Dust: {dust_prob:.2f}%")
